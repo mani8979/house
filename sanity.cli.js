44fbc3c993c24ddb058c54a0df4f538c9957193c
mani8979 <manikalla890@gmail.com>
2026-05-21 10:12:46 +0530

    Fix Sanity Studio deploy env variables

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,8 +2,8 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'wzyfwfrj',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   },
   deployment: {
     appId: 'pwq526alnso3eh7yq7fgvtga',
--- sanity.config.js
+++ sanity.config.js
@@ -3,8 +3,8 @@ import { schema } from './src/sanity/schema';
 
 export default defineConfig({
   basePath: '/studio',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'wzyfwfrj',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   title: 'HouseStudio Interiors',
   schema: schema,
 });
