a2928034df89a5eb9455115bfbad3b9834fbd814
mani8979 <manikalla890@gmail.com>
2026-05-22 15:27:19 +0530

    Generate true favicon.ico to force Google Search recognition

---
+++ scripts/make-ico.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const pngToIco = require('png-to-ico');
+
+pngToIco('d:/house/src/app/icon.png')
+  .then(buf => {
+    fs.writeFileSync('d:/house/src/app/favicon.ico', buf);
+    console.log('Successfully generated favicon.ico');
+  })
+  .catch(console.error);
